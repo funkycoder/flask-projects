ff321d7c6e2823257f04859d25b7720cf56a76d2
funkycoder <bquan2009@gmail.com>
2020-04-05 17:06:01 +0700

    Update delete for appointments

--- app/sched/routes.py
+++ app/sched/routes.py
@@ -6,26 +6,30 @@ from app.sched import appointment
 from app.sched.models import Appointment
 from app.sched.forms import AppointmentForm
 
+
 @appointment.route('/appointment/')
 @login_required
 def appointment_list():
     appts = Appointment.query.filter_by(
         user_id=current_user.id).order_by(Appointment.start.asc()).all()
-    return render_template('sched.html', appts=appts, user_id=current_user.id)
+    if not appts:
+        flash('You don\' have any appointments. Please create one!')
+        return redirect(url_for('appointment.create_appointment'))
+    return render_template('sched.html', appts=appts, user_id=current_user.id, user_name=current_user.user_name.title())
 
 
 @appointment.route('/appointment/create/', methods=['GET', 'POST'])
 @login_required
 def create_appointment():
     form = AppointmentForm()
-    
+
     if form.validate_on_submit():
         appointment = Appointment(
-            title = form.title.data, 
+            title=form.title.data,
             start=form.start.data,
-            end=form.end.data, 
+            end=form.end.data,
             allday=form.allday.data,
-            location=form.location.data, 
+            location=form.location.data,
             description=form.description.data,
             user_id=current_user.id)
 
@@ -37,18 +41,19 @@ def create_appointment():
         return(redirect(url_for('appointment.appointment_list')))
     return render_template('create_appt.html', form=form)
 
+
 @appointment.route('/appointment/edit/<appt_id>', methods=['GET', 'POST'])
 @login_required
 def edit_appointment(appt_id):
     appt = Appointment.query.get(appt_id)
     form = AppointmentForm(obj=appt)
     if form.validate_on_submit():
-        appt.title = form.title.data 
-        appt.start=form.start.data
-        appt.end=form.end.data
-        appt.allday=form.allday.data
-        appt.location=form.location.data
-        appt.description=form.description.data
+        appt.title = form.title.data
+        appt.start = form.start.data
+        appt.end = form.end.data
+        appt.allday = form.allday.data
+        appt.location = form.location.data
+        appt.description = form.description.data
 
         db.session.add(appt)
         db.session.commit()
@@ -56,3 +61,16 @@ def edit_appointment(appt_id):
         flash('Apointment edited successfully!')
         return(redirect(url_for('appointment.appointment_list')))
     return render_template('edit_appt.html', form=form)
+
+
+@appointment.route('/appointment/delete/<appt_id>', methods=['GET', 'POST'])
+@login_required
+def delete_appointment(appt_id):
+    appt = Appointment.query.get(appt_id)
+    if request.method == 'POST':
+        db.session.delete(appt)
+        db.session.commit()
+        flash("Appointment deleted successfully!")
+        return redirect(url_for('appointment.appointment_list'))
+
+    return render_template('delete_appt.html', appt=appt)
